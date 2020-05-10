06861ca8180e52e866dce8eb8299735b673e5bca
axl-codes <axl.lanca@gmail.com>
2020-05-10 02:37:38 +0200

    added JS and CSS

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function randomColor (){
 
 /* Function to generate a div and a span to get the students icons. 
 The parameter is the number of people we want. I've put a parameter to make the code dynamic oh oh oh 
-Function between parentesis : self calling function, we declare the function and we call it at the same time */
+Function between parentesis : self calling function, we declare the function and we call it at the same time (TEST) */
 
 (function studentList (number){
     let peopleList = document.getElementById("people");
@@ -31,11 +31,11 @@ Function between parentesis : self calling function, we declare the function and
         spanIcon.innerHTML = "face";
         people.append(spanIcon);
 
-        //Desable the contextmenu (right click) in all the div 
+        //Cancel the contextmenu (right click) in all the div 
         people.addEventListener('contextmenu', e => {
             e.preventDefault();
-    });
-}
+        });
+    }
 })(30);
 
 
@@ -47,9 +47,11 @@ function getForm(){
     let choiceForm = document.createElement("form");
     let selectForm = document.createElement("select");
     let submit = document.createElement("input");
+    choiceForm.setAttribute("id", "submit");
     choiceForm.append(selectForm);
     submit.value = "Ok";
     submit.style.cursor = "pointer";
+    
     submit.setAttribute("type", "submit");
     // Creation of the select's options
     let specialty = ["Sélectionner", "Developpement", "Design", "Marketing"];
@@ -62,11 +64,10 @@ function getForm(){
     }
 
     choiceForm.append(submit);
+    choiceForm.style.display = 'block';
 
     return choiceForm;
 }
-
-
 
 
 /* Function to create a new form to the right click in a div */ 
@@ -76,22 +77,64 @@ function formAtClick(){
     const newForm = getForm();
 
     for( let i = 0; i < listPeople.length; i++){
-    listPeople[i].addEventListener("mousedown", function(e){ // Mousedown : when a pointing device button is pressed while the pointer is inside the element.
-        if (e.button === 2){ // 2 = the right click (0 the left click, 1 center)
-            // Having the mouse coordinates  
-            var y = e.clientY;
-            var x = e.clientX;
-            console.log(y + " " + x)
-            // using the coordinates to call the form at the same place of the click mouse
-            newForm.style.top = y / 2+ "px";
-            newForm.style.left = x / 2+ "px";
-            this.append(newForm);
-        }
+        listPeople[i].addEventListener("mousedown", function(e){ // Mousedown : when a pointing device button is pressed while the pointer is inside the element.
+            if (e.button === 2){ // 2 = the right click
+                // Having the mouse coordinates  
+                var y = e.clientY;
+                var x = e.clientX;
+                // using the coordinates to call the form at the same place of the click mouse
+                newForm.style.top = y+ "px";
+                newForm.style.left = x + "px";
+                newForm.style.display = 'inline-block';
+                this.append(newForm);
+                
+                
+            }
+        });
+    }
+
+    //Use of preventDefault to cancel the submit event, because we don't want to reload the page
+    newForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        // this.style.display = 'none';
+        changeDiv();
     });
-}
+    
 }
 
 
+function changeDiv(){
+
+    let value = document.querySelector('select').value;
+    let divId = document.querySelector('form').parentNode;
+    const form = document.querySelector('form');
+
+    switch (value){
+        case 'Developpement' :
+            document.querySelector('#dev').append(divId);
+            form.style.display = 'none';
+            form.reset();
+            break;
+
+        case 'Design' : 
+            document.querySelector('#design').append(divId);
+            form.style.display = 'none';
+            form.reset();
+            break;
+
+        case 'Marketing' : 
+            document.querySelector('#mkt').append(divId);
+            form.style.display = 'none';
+            form.reset();
+            break;
+
+        case 'Sélectionnez' :
+        default:
+            alert("Merci de sélectionner une spécialité");
+    }
+
+    
+}
 
 
 
@@ -101,10 +144,12 @@ formAtClick();
 
 
 
+
 /*
 * Manque à faire :
 *   - Récupérer value du select
 *   - Déplacer la div dans la bonne spé
+*   - Trouver comment "refresh" le formulaire
 */
 
 
